7f95cfa21393efc7e9b802e1c5d4fb34d778545e
Joshaharonoff <josh.aharonoff@gmail.com>
2015-08-07 09:26:03 -0400

    Blog changes

--- index.php
+++ index.php
@@ -2,6 +2,20 @@
   include('includes/header.php'); 
 ?>
    <div class='blog_preview'>
+    <p>Aug 6,2015</p>
+    <h1>The Corporate Leap: The Advice That Helped Shape My Decision to Leave Corporate Accounting and Run a Startup</h1>
+    <a href ='http://joshaharonoff.com/blog/the-corporate-leap.html'>
+    <img src='img/corporate-leap.jpg'></a>
+            <p>The clock hit midnight. I was still in my office chair, looking back and forth between the takeout food I had eaten for dinner and the computer screen. Copy + Paste. Repeat 100+ times until work is ready for manager review. It was only then that the thought crept in my mind for the first time....”What if I really left this place and devoted a year to our startup?”</p>
+      <div class='blog_readmore_outer'>
+        <a href='http://joshaharonoff.com/blog/the-corporate-leap.html'>
+         <div class='blog_readmore_inner'>
+          <p>Read More</p> 
+         </div> 
+         </a>
+      </div> 
+   </div> 
+  <div class='blog_preview'>
     <p>Jul 15,2015</p>
     <h1>5 Reasons Why Learning To Code Has Changed My Life</h1>
     <a href ='http://joshaharonoff.com/blog/5-reasons-why-learning-to-code-has-changed-my-life.html'>
